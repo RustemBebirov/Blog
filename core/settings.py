4df03205188fd703ade50e9fc868496503e886d8
rustem <bebirov@bk.ru>
2022-10-23 14:51:45 +0400

    update

--- core/settings.py
+++ core/settings.py
@@ -166,6 +166,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_ACCESS_KEY_ID = 'AKIA4SPBW36LP6YR2X2V'
 AWS_S3_SECRET_ACCESS_KEY = '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'
-AWS_S3_CUSTOM_DOMAIN = 'my-blog-rustem.s3.eu-north-1.amazonaws.com'
+AWS_S3_CUSTOM_DOMAIN = 'my-blog-rustem.s3.amazonaws.com'
 
 AWS_STORAGE_BUCKET_NAME = 'my-blog-rustem'
