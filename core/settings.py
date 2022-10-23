ba05bed012f1b915c1b1ab1c487aea803099deb3
rustem <bebirov@bk.ru>
2022-10-23 12:27:38 +0400

    update

--- core/settings.py
+++ core/settings.py
@@ -140,6 +140,21 @@ USE_I18N = True
 USE_TZ = True
 
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
